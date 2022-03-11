7ea17a41a11452ba9b1e323006da095e7a837e83
abarkdull <abarkdull@gmail.com>
2022-03-11 13:03:05 -0700

    add Navbar.js and Font-Awesome styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
     return (
-        <div className='container-fluid' style={{ backgroundImage: 'url(moroccan-flower-dark.png' }}>
-            <section id='title'>
-                <div className='row' style={{ padding: "20% 15%" }}>
-                    <SearchBar />
-                </div>
-            </section>
+        <div style={{ backgroundImage: 'url(moroccan-flower-dark.png) '}}>
+            <Navbar />
+            <div className='container-fluid' style={{ backgroundColor: 'transparent' }}>
+                <section id='title'>
+                    <div className='row' style={{ padding: "20% 15%" }}>
+                        <SearchBar />
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./app"
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <App />,
